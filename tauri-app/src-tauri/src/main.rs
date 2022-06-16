15bae248c4a36f1258417df981366642c15e90a2
n4to4 <n4to4k@gmail.com>
2022-06-17 07:15:52 +0900

    wip

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -1,12 +1,18 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
-    .run(context)
-    .expect("error while running tauri application");
+    let context = tauri::generate_context!();
+    tauri::Builder::default()
+        .menu(tauri::Menu::os_default(&context.package_info().name))
+        .invoke_handler(tauri::generate_handler![hello_tauri])
+        .run(context)
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn hello_tauri() -> String {
+    "Hello Tauri 1.0".into()
 }
